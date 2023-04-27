41a5faebc24516dc2db96e649a00b96ca1f09ddf
zhangzhong <zhong.zhang@duom.com>
2023-04-27 17:37:15 +0800

    sticker1

---
+++ sticker.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhangzhong zhong.zhang@duom.com
+ * @Date: 2023-04-27 17:36:53
+ * @LastEditors: zhangzhong zhong.zhang@duom.com
+ * @LastEditTime: 2023-04-27 17:37:00
+ * @FilePath: /stanlyTest/sticker.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 1
