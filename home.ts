e74f41317e1a9833ed3bd7bf2804cae0084f7c1b
zhangzhong <zhong.zhang@duom.com>
2023-04-27 17:38:04 +0800

    home 1

---
+++ home.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhangzhong zhong.zhang@duom.com
+ * @Date: 2023-04-27 17:37:50
+ * @LastEditors: zhangzhong zhong.zhang@duom.com
+ * @LastEditTime: 2023-04-27 17:37:56
+ * @FilePath: /stanlyTest/home.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// home 1
+
